08a3b67e206b1f9f302bcf2a4e2aae0d88e42eae
Ricardo Avila <ricdex@gmail.com>
2021-06-16 19:05:41 -0500

    keynua

--- src/persona/empresa.service.ts
+++ src/persona/empresa.service.ts
@@ -3,6 +3,7 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Empresa } from './entities/empresa.entity';
 import { ConfigService } from '@nestjs/config';
+import { pdf2base64 } from 'pdf-to-base64';
 
 @Injectable()
 export class EmpresaService {
@@ -20,9 +21,50 @@ export class EmpresaService {
   async registrar(empresa: Empresa): Promise<Empresa> {
     //enviar correo
     const secretKey = this.configService.get('SENDGRID_API_KEY');
+    const emailUrl = this.configService.get('SENDGRID_PATH');
     const templateKey = this.configService.get('TEMPLATE_ID');
     const emailSender = this.configService.get('EMAIL_SENDER');
+    const keynuaPath = this.configService.get('KEYNUA_PATH');
+    const keynuaKey = this.configService.get('KEYNUA_KEY');
+    const keynuaToken = this.configService.get('KEYNUA_TOKEN');
+    const keynuaLink = this.configService.get('KEYNUA_LINK');
+    const documentPDF = this.configService.get('CONTRACT_PDF_URL');
 
+    const base64Str = await pdf2base64(documentPDF);
+    //generacion del link
+    const headersRequestKeynua = {
+      'Content-Type': 'application/json',
+      authorization: `${keynuaToken}`,
+      'x-api-key': `${keynuaKey}`,
+    };
+    const dataKeynua = {
+      title: 'Contrato de registro en FFC',
+      languague: 'es',
+      documents: [
+        {
+          name: 'CONTRATO_SERVICIOS_FFC.pdf',
+          base64: base64Str,
+        },
+      ],
+      users: [
+        {
+          name:
+            empresa.representanteLegal.nombre +
+            ' ' +
+            empresa.representanteLegal.apellido,
+          groups: ['signers'],
+        },
+      ],
+    };
+    const result = await this.httpService
+      .post(`${keynuaPath}/contracts/v1`, dataKeynua, {
+        headers: headersRequestKeynua,
+      })
+      .toPromise();
+
+    const tokenLinkUrl = result.data.users[0].token;
+
+    //generacion del correo
     const headersRequest = {
       'Content-Type': 'application/json', // afaik this one is not needed
       Authorization: `Bearer ${secretKey}`,
@@ -41,6 +83,8 @@ export class EmpresaService {
           dynamic_template_data: {
             nombre: empresa.razonSocial.toUpperCase(),
             tipo: 'Empresa',
+            dni: empresa.usuario.correo,
+            link: `${keynuaLink}?token=${tokenLinkUrl}`,
           },
         },
       ],
@@ -48,8 +92,9 @@ export class EmpresaService {
       template_id: templateKey,
     };
 
+    //enviar correo
     await this.httpService
-      .post('https://api.sendgrid.com/v3/mail/send', data, {
+      .post(`${emailUrl}`, data, {
         headers: headersRequest,
       })
       .toPromise();
